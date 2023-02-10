aa92fd8324a383d362274e70de412640ad45d731
Haley Akin <wasatchtimpanogos@outlook.com>
2023-02-10 10:30:40 -0700

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { useState } from "react";
+import UseMediaQuery from "./hooks/UseMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('Home')
+  const isAboveMediumScreens = UseMediaQuery("(min-width: 1060px)")
+
+
   return (
-    <div className="App">
-     
+    <div className="app bg-deep-blue">
+     <Navbar
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+     />
+
+
+
     </div>
   );
 }
---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import UseMediaQuery from "../hooks/UseMediaQuery"
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+    const lowerCasePage = page.toLowerCase()
+    return (
+        <AnchorLink
+          className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+            hover:text-yellow transition-duration-500`}
+          href={`#${lowerCasePage}`}
+          onClick={() => {setSelectedPage(lowerCasePage)}}
+        >
+           {page} 
+        </AnchorLink>
+    )
+}
+
+
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+    const [isMenuToggled, setIsMenuToggled] = useState(false)
+    const isAboveSmallScreens = UseMediaQuery("(min-width: 768px)")
+
+    return(
+        <nav className={`z-40 w-full fixed top-0 py-6`}>
+            <div className="flex items-center justify-between mx-auto w-5/6"> 
+                <h4 className="font-playfair text-3xl font-bold">JE</h4>
+
+                {/* Desktop Navbar */}
+                {isAboveSmallScreens ? (
+                    <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">  
+                        <Link 
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                ) : (<div></div>)}
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar
